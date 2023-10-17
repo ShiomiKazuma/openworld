e948601932893dbcb667d047eeb93c68e5b6d690
KentoMatsuzaki <vantanstudent@outlook.jp>
2023-10-17 13:40:44 +0900

    [fix] プレイヤーの挙動

--- Assets/Matsuzaki/Scripts/Player.cs
+++ Assets/Matsuzaki/Scripts/Player.cs
@@ -7,24 +7,26 @@ public class Player : MonoBehaviour
     [SerializeField] private Animator _animator;
     [SerializeField] private InputKeyboard _inputKeyboard;
     [SerializeField] private float _moveSpeed = 5.0f;
+    [SerializeField] private Camera _playerCamera = null;
+    Rigidbody _rb;
 
     private void Awake()
     {
         _inputKeyboard.OnStartEvent = () => _animator.SetBool("IsMove", true);
         _inputKeyboard.OnCancelEvent = () => _animator.SetBool("IsMove", false);
+        _rb = GetComponent<Rigidbody>();
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        if(_inputKeyboard.IsInput)
+        if (_inputKeyboard.IsInput)
         {
             Move(_inputKeyboard.Vector);
         }
     }
     private void Move(Vector2 vector)
     {
-        var moveVector = new Vector3(vector.x, 0f, vector.y) * _moveSpeed * Time.deltaTime;
-        var movePosition = transform.position + moveVector;
-        transform.LookAt(movePosition);
-        transform.position = movePosition;
+        var moveVector = new Vector3(vector.x, 0f, vector.y).normalized;
+        //Debug.Log(vector.x + " " + vector.y);
+        _rb.AddForce(moveVector * _moveSpeed, ForceMode.Force);
     }
 }
