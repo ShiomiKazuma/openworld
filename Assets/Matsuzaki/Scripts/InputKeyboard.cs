99195352196f3af43346363b801a68be4d6e051d
KentoMatsuzaki <vantanstudent@outlook.jp>
2023-10-17 10:21:43 +0900

    [add] InputKeyboard.cs, Player.cs

---
+++ Assets/Matsuzaki/Scripts/InputKeyboard.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputKeyboard : MonoBehaviour
+{
+    [SerializeField] InputAction _inputAction;
+
+    Action OnStartEvent, OnCancelEvent;
+
+    Action<Vector2> OnPerformEvent;
+    public bool IsInput { get; private set; }
+    public Vector2 Vector { get; private set; }
+
+    private void OnEnable() => _inputAction.Enable();
+    private void OnDisable() => _inputAction.Disable();
+
+    private void Awake()
+    {
+        _inputAction.started += OnStart;
+        _inputAction.performed += OnPerform;
+        _inputAction.canceled += OnCancel;
+    }
+    private void OnStart(InputAction.CallbackContext context)
+    {
+        IsInput = true;
+        OnStartEvent?.Invoke();
+    }
+    private void OnPerform(InputAction.CallbackContext context)
+    {
+        Vector = _inputAction.ReadValue<Vector2>();
+        OnPerformEvent?.Invoke(Vector);
+    }
+    private void OnCancel(InputAction.CallbackContext context)
+    {
+        IsInput = false;
+        OnCancelEvent?.Invoke();
+    }
+}
